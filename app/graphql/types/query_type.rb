6a676ae3f340f0632c21df15a070a3e0e47ad875
keisukekashimura <kashimura@refcome.com>
2020-11-01 13:03:24 +0900

    add user search query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,13 +1,8 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
-
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :user, Types::UserType, null: false
+    def user
+      context[:current_user]
     end
   end
 end
