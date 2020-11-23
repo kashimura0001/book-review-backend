69705b80522b4ec3b98c67d068fb4e1b41faa75b
keisukekashimura <kashimura@refcome.com>
2020-11-23 22:15:11 +0900

    fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_23_064604) do
+ActiveRecord::Schema.define(version: 2020_11_23_114901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -28,36 +28,18 @@ ActiveRecord::Schema.define(version: 2020_11_23_064604) do
     t.index ["organization_id"], name: "index_books_on_organization_id"
   end
 
-  create_table "organization_invitations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+  create_table "organization_members", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.uuid "organization_id", null: false
-    t.uuid "organization_role_id", null: false
-    t.string "invite_code", limit: 255, null: false
-    t.string "email", limit: 255, null: false
-    t.integer "status", null: false
-    t.datetime "invited_at", null: false
+    t.uuid "user_id"
+    t.integer "role", null: false
+    t.string "invite_code", limit: 255
+    t.string "invite_email", limit: 255
+    t.integer "invite_status"
+    t.datetime "invite_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["invite_code"], name: "index_organization_invitations_on_invite_code", unique: true
-    t.index ["organization_id"], name: "index_organization_invitations_on_organization_id"
-    t.index ["organization_role_id"], name: "index_organization_invitations_on_organization_role_id"
-  end
-
-  create_table "organization_roles", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "role", limit: 100, null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["role"], name: "index_organization_roles_on_role", unique: true
-  end
-
-  create_table "organization_users", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.uuid "organization_id", null: false
-    t.uuid "user_id", null: false
-    t.uuid "organization_role_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["organization_id"], name: "index_organization_users_on_organization_id"
-    t.index ["organization_role_id"], name: "index_organization_users_on_organization_role_id"
-    t.index ["user_id"], name: "index_organization_users_on_user_id"
+    t.index ["organization_id"], name: "index_organization_members_on_organization_id"
+    t.index ["user_id"], name: "index_organization_members_on_user_id"
   end
 
   create_table "organizations", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
@@ -91,11 +73,8 @@ ActiveRecord::Schema.define(version: 2020_11_23_064604) do
   end
 
   add_foreign_key "books", "organizations"
-  add_foreign_key "organization_invitations", "organization_roles"
-  add_foreign_key "organization_invitations", "organizations"
-  add_foreign_key "organization_users", "organization_roles"
-  add_foreign_key "organization_users", "organizations"
-  add_foreign_key "organization_users", "users"
+  add_foreign_key "organization_members", "organizations"
+  add_foreign_key "organization_members", "users"
   add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "organizations"
   add_foreign_key "reviews", "users"
